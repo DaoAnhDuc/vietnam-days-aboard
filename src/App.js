2ad8b76879d77f07ff6f84750ebbbf889a1d3401
Anh Duc <daoanhduc2604@gmail.com>
2024-10-24 22:19:30 +0700

    oke

--- src/App.js
+++ src/App.js
@@ -1,27 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [open, setOpen] = useState(false);
   return (
     <div className="App">
-      <div className="w-full h-20" style={{ borderBottom: "1px solid #ffffff6e" }}>
+      <div className="relative w-full h-20" style={{ borderBottom: "1px solid #ffffff6e" }}>
         <div className="container h-full flex justify-between items-center">
           <img className="h-16" src="./images/logo.png" />
-          <div className="flex font-bold gap-6 items-center" style={{ fontSize: 15 }}>
-            <a className="text-sm" href="#">
-              Trang chủ
-            </a>
-            <a className="text-sm">Ngày Việt Nam ở nước ngoài</a>
-            <a className="text-sm">Tin tức</a>
-            <a className="text-sm">Liên Hệ</a>
-            <a className="text-sm">
-              Triển lãm trực tuyến <br></br> Tranh sơn mài Việt Nam
-            </a>
+          <div className="flex font-bold gap-4 items-center" style={{ fontSize: 15 }}>
+            <div className="lg:flex hidden font-bold gap-6 items-center  ">
+              <a className="text-sm" href="#">
+                Trang chủ
+              </a>
+              <a className="text-sm">Ngày Việt Nam ở nước ngoài</a>
+              <a className="text-sm">Tin tức</a>
+              <a className="text-sm">Liên Hệ</a>
+              <a className="text-sm">
+                Triển lãm trực tuyến <br></br> Tranh sơn mài Việt Nam
+              </a>
+            </div>
             <div className="flex gap-3">
               <img className="h-6" src="./images/vietnam.png" />
               <img className="h-6" src="./images/united-kingdom.png" />
             </div>
+            <button className="lg:hidden flex " onClick={() => setOpen(!open)}>
+              <img className="h-8 cursor-pointer" src="./images/bars-icon.png" />
+            </button>
           </div>
+        </div>
+
+        <div
+          className={`absolute right-0 top-full font-bold bg-white w-full lg:flex lg:items-center lg:space-x-6 lg:mt-0 transition-all duration-500 ease-in-out transform ${
+            open ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-5 pointer-events-none"
+          }`}
+          style={{ boxShadow: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)", borderTop: "1px solid rgba(0,0,0,0.24)" }}
+        >
+          <a className="block lg:inline-block text-sm px-4 py-4 hover:bg-gray-300" href="#">
+            Trang chủ
+          </a>
+          <a className="block lg:inline-block text-sm px-4 py-4 hover:bg-gray-300" href="#">
+            Ngày Việt Nam ở nước ngoài
+          </a>
+          <a className="block lg:inline-block text-sm px-4 py-4 hover:bg-gray-300" href="#">
+            Tin tức
+          </a>
+          <a className="block lg:inline-block text-sm px-4 py-4 hover:bg-gray-300" href="#">
+            Liên Hệ
+          </a>
+          <a className="block lg:inline-block text-sm px-4 py-4 hover:bg-gray-300" href="#">
+            Triển lãm trực tuyến Tranh sơn mài Việt Nam
+          </a>
         </div>
       </div>
       <div>
@@ -83,7 +112,7 @@ function App() {
             <div className="text-black text-sm">Đơn vị phối hợp</div>
             <div style={{ flex: 1, borderBottom: "1px solid #dfdfdf" }}></div>
           </div>
-          <div className="flex justify-center gap-10">
+          <div className="flex justify-center gap-x-10  flex-wrap">
             <img className="h-24" src="./images/logo-01.png" />
             <img className="h-24" src="./images/logo-02.png" />
             <img className="h-24" src="./images/logo-03.png" />
@@ -92,7 +121,7 @@ function App() {
       </div>
       <div className="mt-10 pb-16 pt-16 text-white" style={{ background: "#151748" }}>
         <div className="container">
-          <div className="flex items-center  gap-10 pb-10" style={{ borderBottom: "1px solid #454545" }}>
+          <div className="flex items-center flex-wrap gap-10 pb-10" style={{ borderBottom: "1px solid #454545" }}>
             <img className="h-24" src="./images/logo.png" />
             <div>
               <p className="mt-2">Vui lòng liên lạc để biết thêm thông tin: Vụ Ngoại giao Văn hóa và UNESCO - Bộ ngoại giao Việt Nam</p>
