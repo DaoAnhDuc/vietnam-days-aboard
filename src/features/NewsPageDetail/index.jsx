d7772e1fcc95cdfe4b1f768a75bc9e1e28b06313
Anh Duc <daoanhduc2604@gmail.com>
2024-10-30 22:55:16 +0700

    oke

--- src/features/NewsPageDetail/index.jsx
+++ src/features/NewsPageDetail/index.jsx
@@ -7,6 +7,7 @@ import axios from "axios";
 function NewsPageDetail() {
   const { newsId } = useParams();
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     getData();
     return () => {};
@@ -17,26 +18,29 @@ function NewsPageDetail() {
       const data = await getNews();
       const newsData = data.find((i) => slugify(i.title) === newsId);
       const response = await axios.get(`/${newsData.template}`);
+      setLoading(false);
       setData({
         ...newsData,
         details: response.data,
       });
-    } catch (error) {}
+    } catch (error) {
+      setLoading(false);
+    }
   };
 
   return (
     <div className="">
       <Header />
-        {!data && (
-          <div className="flex flex-col items-center justify-center bg-gradient-to-br from-red-500 to-red-800 text-white" style={{ height: "calc(100vh - 80px)" }}>
-            <h1 className="text-9xl font-bold mb-4 animate-bounce">404</h1>
-            <h2 className="text-3xl font-semibold mb-2">Oops! Trang không tìm thấy.</h2>
-            <p className="text-lg mb-8 text-center max-w-md">Xin lỗi, trang bạn đang tìm kiếm không tồn tại hoặc đã bị di chuyển. Hãy quay lại trang chủ và thử lại.</p>
-            <Link to="/" className="px-8 py-3 bg-white text-red-700 font-semibold rounded-lg shadow-lg hover:bg-gray-200 transition duration-300">
-              Trở về trang chủ
-            </Link>
-          </div>
-        )}
+      {!data && !loading && (
+        <div className="flex flex-col items-center justify-center bg-gradient-to-br from-red-500 to-red-800 text-white" style={{ height: "calc(100vh - 80px)" }}>
+          <h1 className="text-9xl font-bold mb-4 animate-bounce">404</h1>
+          <h2 className="text-3xl font-semibold mb-2">Oops! Trang không tìm thấy.</h2>
+          <p className="text-lg mb-8 text-center max-w-md">Xin lỗi, trang bạn đang tìm kiếm không tồn tại hoặc đã bị di chuyển. Hãy quay lại trang chủ và thử lại.</p>
+          <Link to="/" className="px-8 py-3 bg-white text-red-700 font-semibold rounded-lg shadow-lg hover:bg-gray-200 transition duration-300">
+            Trở về trang chủ
+          </Link>
+        </div>
+      )}
       <div style={{ maxWidth: 680, margin: "0 auto" }}>
         {data && (
           <>
